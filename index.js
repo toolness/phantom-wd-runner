e232d9a43dd84bed0b5b3f364db3a0f702f470ab
Atul Varma <varmaa@gmail.com>
2013-08-13 13:39:31 -0400

    Decoupled phantom-specific code from generic stuff in index.js.

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
+var RUNNER_PATH = __dirname + '/lib/server-runner.js';
+
 var pingUntilReady = require('./lib/ping-until-ready');
 
-var runnerPath = __dirname + '/lib/server-runner.js';
+function startServerAndPing(options) {
+  var runner = require('child_process').fork(RUNNER_PATH, options.cmdline);
 
-module.exports = function(options) {
-  if (!options) options = {};
-
-  var timeout = options.timeout || 15000;
-  var port = (options.port || '4444').toString();
-  var execPath = options.execPath || 'phantomjs';
-  var statusUrl = 'http://localhost:' + port + '/status';
-  var runner = require('child_process').fork(runnerPath, [
-    execPath, '--webdriver=' + port
-  ]);
   runner.on('exit', function(code) {
     if (code)
       runner.emit('error',
-                  new Error('phantomjs runner exited with code ' + code));
+                  new Error("'" + options.cmdline[0] +
+                            "' runner exited with code " + code));
   });
-  pingUntilReady(statusUrl, timeout, function(err) {
+  pingUntilReady(options.url, options.timeout, function(err) {
     if (err) return runner.emit('error', err);
     runner.emit('listening');
   });
 
   return runner;
+}
+
+function startPhantomWebdriver(options) {
+  if (!options) options = {};
+
+  var port = (options.port || '4444').toString();
+
+  return startServerAndPing({
+    timeout: options.timeout || 15000,
+    url: 'http://localhost:' + port + '/status',
+    cmdline: [options.execPath || 'phantomjs', '--webdriver=' + port]
+  });
 };
 
+module.exports = startPhantomWebdriver;
+
 if (!module.parent)
-  module.exports().on("listening", function() {
+  startPhantomWebdriver().on("listening", function() {
     console.log("PhantomJS webdriver server ready! Exiting...");
     this.kill();
   });
